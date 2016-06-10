9a5ba7ef6f43c72f30ff80024ab8d1ace43bf33e
m.abdelkafy <m.abdelkafy@gmail.com>
2016-06-10 02:03:03 +0200

    Create GigTable, OverrideConventions, populate genres, GigsController, View and ViewModel

---
+++ GigHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GigHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
